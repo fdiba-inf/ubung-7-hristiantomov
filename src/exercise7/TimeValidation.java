70da566e14a1e493c488a232f803d1b31ee0d2f0
hristiantomov <>
2022-01-02 15:30:28 +0000

    Some message

--- src/exercise7/TimeValidation.java
+++ src/exercise7/TimeValidation.java
@@ -5,26 +5,26 @@ import java.util.Scanner;
 public class TimeValidation {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+           
         System.out.print("Enter hours: ");
         int hours = input.nextInt();
 
         System.out.print("Enter minutes: ");
         int minutes = input.nextInt();
 
-        boolean timeValid = false; // Call validation methods
+        boolean timeValid = areHoursValid(hours) && areMinutesValid(minutes);
         System.out.println("Time valid: " + timeValid);
     }
 
 
     public static boolean areHoursValid(int hours) {
-        // Implement method
-        return false;
+        
+        return hours >= 0 && hours <= 24;
     }
 
 
     public static boolean areMinutesValid(int minutes) {
-        // Implement method
-        return false;
+        
+        return minutes >= 0 && minutes <= 60;
     }
 }
